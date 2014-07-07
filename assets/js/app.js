02d1f15c2d74da16790dabc01755dec2d27aaca6
bradfordbradford <bradford@wearetelegraph.com>
2014-07-07 17:16:43 -0500

    Tidy files, Latest Sermon for Nav Menu, New Favicon

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,25 +1,28 @@
-
-
-
-//  // Now, Now  //
 $(document).ready(function($) {
 
-    // Prevent FOUC while JS loads, then swap for Run ----------
+
+
+    // Prevent FOUC while JS loads, then swap for Run
+    // -----------------------------------------------
     $('html').removeClass('no-js').addClass('js');
     setTimeout(function () {
         $('body').removeClass('init').addClass('run');
     }, 500);
 
-    // Menu ----------
+
+
+    // Menu
+    // -----------------------------------------------
     $('#main-navigation').accessibleMegaMenu();
     $('#main-navigation').addClass('run');
 
 
-    // Mobile Menu ----------
+
+
+    // Mobile Menu
+    // -----------------------------------------------
     var menu = $('#navigation-menu');
     var menuToggle = $('#js-mobile-menu');
-    var signUp = $('.sign-up');
-
     $(menuToggle).on('click', function(e) {
     e.preventDefault();
     $(this).toggleClass("active-menu");
@@ -31,12 +34,14 @@ $(document).ready(function($) {
     });
 
 
+
+
     // Search
+    // -----------------------------------------------
     $('.js-search-trigger').on('click touchstart', function(e){
       $('.js-search').toggleClass('is-visible');
       $('#js-search-overlay').addClass('is-visible');
       e.preventDefault();
-
     });
     $('#js-search-overlay').on('click touchstart', function(e){
       $('.js-search').removeClass('is-visible');
@@ -44,24 +49,11 @@ $(document).ready(function($) {
       e.preventDefault();
     });
 
-    // $('body').on('click touchstart', function(e){
-    //   $('.js-search').removeClass('is-visible');
-    //   e.preventDefault();
-    // });
-
-
-    // underline under the active nav item
-    $(".nav .nav-link").click(function() {
-    $(".nav .nav-link").each(function() {
-      $(this).removeClass("active-nav-item");
-    });
-    $(this).addClass("active-nav-item");
-    $(".nav .more").removeClass("active-nav-item");
-    });
 
 
 
-    // Smooth Scroll for Basic Anchors Links ----------
+    // Smooth Scroll for Basic Anchors Links
+    // -----------------------------------------------
     $('a[href^="#"]').on('click',function (e) {
       e.preventDefault();
       var target = this.hash,
@@ -74,7 +66,11 @@ $(document).ready(function($) {
       });
     });
 
+
+
+
     // Horizontal Tabs
+    // -----------------------------------------------
      $('.accordion-tabs-minimal').each(function(index) {
         $(this).children('li').first().children('a').addClass('is-active').next().addClass('is-open').show();
       });
@@ -95,57 +91,14 @@ $(document).ready(function($) {
 
 
 
-    // // Toggle View All ----------
-    // $('.open-g-door').on('click', function(e){
-    //     $('.g-door').toggleClass("open");
-    //     mainNav.toggleClass("push-for-door");
-    //     $(this).toggleClass("switch");
-    //     e.preventDefault();
-    // });
-    // // Add an optional secondary close button
-    // $('.close-g-door').on('click', function(e){
-    //     $('.g-door').removeClass("open");
-    //     mainNav.removeClass("push-for-door");
-    //     e.preventDefault();
-    // });
 
-
-    // LazyLoad Images ----------
+    // LazyLoad Images
+    // -----------------------------------------------
     $('img.load').lazyload({
         effect : "fadeIn"
     });
 
 
-    // Collapsible Content - Add Toggle ----------
-    // var toggleIcon = $( "<span class='toggle-icon' data-icon='E'></span>" );
-    // $('.collapse-header').append(toggleIcon);
-    // // Collapsible Content
-    // $(function() {
-    //   $(".collapse-header").click(function () {
-    //     if ($(this).hasClass('open')){
-    //         $(this).next('.collapse-content').slideUp('slow');
-    //         $(this).removeClass('open')
-    //     }
-    //     else {
-    //       // close other content
-    //       $('.collapse-header').not(this).next('.collapse-content').slideUp('slow');
-    //       $('.collapse-header').not(this).removeClass('open');
 
-    //       //open clicked content
-    //       $(this).next('.collapse-content').slideDown('slow');
-    //       // add open class
-    //       $(this).addClass('open');
-    //     }
-    //   });
-    // });
-
-
-    // Slideshow ----------
-    // $('.slideshow').flexslider({
-    //     animation: "slide",
-    //     controlNav: false,
-    //     prevText: '',
-    //     nextText: ''
-    // });
 
 });
